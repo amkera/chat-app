a9b6ab2018a8220ca2df766453244850b1280815
amkera <amber_kerawala@berkeley.edu>
2022-07-20 15:47:39 -0400

    Add get messages service

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ let app = express()
 
 app.use(express.static(__dirname)) //the whole path, which is /Users/ambertorres/code/flex-time/chat-app
 
+let messages = [
+  { name: "Tim", message: "Hello" },
+  { name: "Jane", message: "Hello" },
+]
+
+app.get("/messages", (req, res) => {
+  res.send(messages)
+})
+
 let server = app.listen(3000, () =>
   console.log("server is listening on port ", server.address().port)
 )
